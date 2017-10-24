a8559a4f61f6446718e3509fbcb2782c4cc0dbec
Juan Lopes <me@juanlopes.net>
2017-10-24 21:38:01 -0200

    equal

--- equal.cpp
+++ equal.cpp
@@ -3,20 +3,20 @@
 #include<iomanip>
 using namespace std;
 
-int T[10010];
+long long T[10010];
 
 int main() { 
     int test; cin >> test;
     int N;
     while(cin >> N) {
-        int minn = 1000000;
+        long long minn = 100000000000ll;
         for(int i=0; i<N; i++) {
             cin >> T[i];
             minn = min(minn, T[i]);
         }
     
         long long result = 1000000000000ll;
-        for(int bias=0; bias<6 && minn-bias >= 0; bias++) {
+        for(int bias=0; bias<10; bias++) {
             long long total = 0;
             for(int i=0; i<N; i++) {
                 int v = T[i] - minn + bias;
